fce6f5ffffb9107be3357a8e662c40ff5b13846e
lechiffre4 <toxcoa@gmail.com>
2021-10-20 12:32:22 +0200

    Cam Smoothing doesn't work

--- Projet prod/Assets/Scripts/Camera/CameraController.cs
+++ Projet prod/Assets/Scripts/Camera/CameraController.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject Plane;
-    public GameObject Camera;
+   /* public GameObject Plane;
+    public Camera Camera;
 
     void Update()
     {
-    Vector3 moveCamto = Plane.transform.position - Plane.transform.forward * 10.0f + PlaneController.transform.up * 5.0f;
-    Camera.main.transform.position = moveCameTo;
+    Vector3 moveCamto = Plane.transform.position - Plane.transform.forward * 10.0f + Plane.transform.up * 5.0f;
+    Camera.main.transform.position = moveCamto;
+    Camera.main.transform.LookAt(Plane.transform.position);
+    }*/
+
+    // Target to follow
+    [SerializeField]
+    Transform targetTransform;
+
+    // Offset vector
+    [SerializeField]
+    Vector3 offsetPosition = new Vector3(0, 7, -18);
+
+    // Rotation parameter
+    [SerializeField]
+    Vector3 offsetRotation = new Vector3(12, 0, 0);
+
+    private void FixedUpdate()
+    {
+        Vector3 position = targetTransform.position + targetTransform.rotation * offsetPosition;
+        Quaternion rotation = targetTransform.rotation * Quaternion.Euler(offsetRotation);
+
+        //transform.rotation = Mathf.SmoothDamp(rotation,,Time.fixedDeltaTime);
+        transform.position = position;
     }
 }
