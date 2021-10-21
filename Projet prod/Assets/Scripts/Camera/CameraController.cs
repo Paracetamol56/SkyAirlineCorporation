6109509b07205ff80ab203a9398d53c07e0d96e9
Mathéo <matheo.galu56@gmail.com>
2021-10-21 10:42:09 +0200

    Smooth camera

--- Projet prod/Assets/Scripts/Camera/CameraController.cs
+++ Projet prod/Assets/Scripts/Camera/CameraController.cs
@@ -16,22 +16,28 @@ public class CameraController : MonoBehaviour
 
     // Target to follow
     [SerializeField]
-    Transform targetTransform;
+    private Transform targetTransform;
 
     // Offset vector
     [SerializeField]
-    Vector3 offsetPosition = new Vector3(0, 7, -18);
+    private Vector3 offsetPosition = new Vector3(0, 7, -18);
 
     // Rotation parameter
     [SerializeField]
-    Vector3 offsetRotation = new Vector3(12, 0, 0);
+    private Vector3 offsetRotation = new Vector3(12, 0, 0);
+
+    // Smoothing effect
+    [SerializeField]
+    private float smoothness;
+
+    private Vector3 cameraVelocity;
 
     private void FixedUpdate()
     {
-        Vector3 position = targetTransform.position + targetTransform.rotation * offsetPosition;
+        Vector3 position = Vector3.SmoothDamp(transform.position, targetTransform.position + targetTransform.rotation * offsetPosition, ref cameraVelocity, smoothness);
         Quaternion rotation = targetTransform.rotation * Quaternion.Euler(offsetRotation);
 
-        //transform.rotation = Mathf.SmoothDamp(rotation,,Time.fixedDeltaTime);
         transform.position = position;
+        transform.rotation = rotation;
     }
 }
