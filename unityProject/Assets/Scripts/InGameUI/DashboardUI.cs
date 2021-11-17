a021d7ca61f3aaedfc5edd66bdbe31e3cedef6ac
Mathéo <matheo.galu56@gmail.com>
2021-11-17 21:55:20 +0100

    Altivariometer implemented

--- unityProject/Assets/Scripts/InGameUI/DashboardUI.cs
+++ unityProject/Assets/Scripts/InGameUI/DashboardUI.cs
@@ -72,12 +72,12 @@ public class DashboardUI : MonoBehaviour
         speedText.text = Mathf.Round(Mathf.Clamp(airPlaneRigidbody.velocity.magnitude, 0.0f, 200.0f)).ToString();
 
         // RPM counter
-        Debug.Log(airPlaneController.GetThrottle());
         rpmCounter.rectTransform.localRotation = Quaternion.Euler(0, 0, airPlaneController.GetThrottle() / 200 * -252 + 126);
 
         // Altivariometer
-
+        altivariometer.rectTransform.localRotation = Quaternion.Euler(0, 0, -airPlaneRigidbody.velocity.y + 90);
 
         // Altimeter
+        // 1 meter = 3.28 feet
     }
 }
