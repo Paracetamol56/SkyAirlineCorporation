57e24baa32c993e05dd0be542a0659d86c5a69e8
Mathéo <matheo.galu56@gmail.com>
2021-11-23 18:27:11 +0100

    Waypoint works

--- unityProject/Assets/Scripts/Waypoint/Waypoint.cs
+++ unityProject/Assets/Scripts/Waypoint/Waypoint.cs
@@ -1,8 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 class Waypoint : MonoBehaviour
 {
+    [SerializeField]
+    private Camera camera;
+    [SerializeField]
+    private TextMeshProUGUI distanceText;
+    [SerializeField]
+    private RectTransform Overlay;
 
+    private void Update()
+    {
+        // If the waypoint if behind the player
+        if (Vector3.Dot(transform.position - camera.transform.position, camera.transform.forward) < 0)
+        {
+            // Hide the overlay
+            Overlay.gameObject.SetActive(false);
+        }
+        else
+        {
+            // Show the overlay
+            Overlay.gameObject.SetActive(true);
+            Vector3 screenPos = camera.WorldToScreenPoint(transform.position);
+            screenPos.z = 0;
+            Overlay.transform.position = screenPos;
+            distanceText.text = (Vector3.Distance(transform.position, camera.transform.position) / 16093).ToString("F2");
+        }
+    }
 }
