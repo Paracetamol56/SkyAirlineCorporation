962ea6c380e5ad0b7fb9479c894d6427030114a8
Thibaud CB <thibaud.cb@outlook.fr>
2021-11-26 15:59:30 +0100

    Fire and water interaction

---
+++ unityProject/Assets/Scripts/FFPlaneMode/PartSystem.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PartSystem : MonoBehaviour
+{
+    public List<ParticleCollisionEvent> collisionEvents;
+    public ParticleSystem part;
+
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+        int i = 0;
+        Debug.Log(other.tag);
+        
+
+        while (i < numCollisionEvents)
+        {
+            if (other.tag == "Fire")
+            {
+                Debug.Log("je suis touché");
+                other.SetActive(false);
+            }
+            i++;
+        }
+    }
+}
