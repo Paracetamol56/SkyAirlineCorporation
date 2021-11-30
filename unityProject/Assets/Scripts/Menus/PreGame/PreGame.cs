95cc519177971f9c2b63d7bf70f00d2eb58688aa
Mathéo <matheo.galu56@gmail.com>
2021-11-30 14:34:57 +0100

    URP transition

--- unityProject/Assets/Scripts/Menus/PreGame/PreGame.cs
+++ unityProject/Assets/Scripts/Menus/PreGame/PreGame.cs
@@ -5,102 +5,102 @@ using UnityEngine.SceneManagement;
 
 public class PreGame : MonoBehaviour
 {
-    [SerializeField] GameObject[] planesSelection;
-    [SerializeField] int rotationSpeed = 10;
-    public int coef = 1;
-    private GameObject currentPlaneShown;
-    private int planeIndex = 0;
-    private GameObject showPoint;
-    private GlobalGameManager.listOfPlanes currentPlaneType;
-    private Quaternion planeAngle;
+  [SerializeField] GameObject[] planesSelection;
+  [SerializeField] int rotationSpeed = 10;
+  public int coef = 1;
+  private GameObject currentPlaneShown;
+  private int planeIndex = 0;
+  private GameObject showPoint;
+  private GlobalGameManager.listOfPlanes currentPlaneType;
+  private Quaternion planeAngle;
 
-    // Instances of managers
-    private GlobalGameManager gm;
-    private ManagerScene ms;
+  // Instances of managers
+  private GlobalGameManager gm;
+  private ManagerScene ms;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    showPoint = GameObject.Find("ShowPoint");
+    planesSelection[planeIndex].transform.localScale = new Vector3(1, 1, 1) * coef;
+    currentPlaneShown = Instantiate(planesSelection[planeIndex], showPoint.transform);
+
+    gm = GlobalGameManager.GetInstance();
+    ms = ManagerScene.instance;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    currentPlaneShown.transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+    planeAngle = currentPlaneShown.transform.rotation;
+  }
+
+  public void StartGame()
+  {
+    SaveCurrentPlane();
+
+    gm.SetSelectedPlane(currentPlaneType);
+
+    //SceneManager.LoadScene("Presentation", LoadSceneMode.Single);
+
+    ms.SetMode(ManagerScene.GameMode.Presentation);
+    ms.LoadGameScene();
+
+    //ms.SetMode(ManagerScene.GameMode.Freemode);
+    //ms.LoadGameScene();
+  }
+
+  public void ExitGame()
+  {
+    //ms.Quit();
+  }
+
+  public void NextPlane()
+  {
+    planeIndex++;
+    planeIndex = planeIndex > planesSelection.Length - 1 ? 0 : planeIndex;
+    UpdateShownPlane();
+  }
+
+  public void PreviousPlane()
+  {
+    planeIndex--;
+    planeIndex = planeIndex < 0 ? planesSelection.Length - 1 : planeIndex;
+    UpdateShownPlane();
+  }
+
+  private void UpdateShownPlane()
+  {
+    Destroy(currentPlaneShown);
+    currentPlaneShown = planesSelection[planeIndex];
+    currentPlaneShown.transform.localScale = new Vector3(1, 1, 1) * coef;
+    currentPlaneShown = Instantiate(currentPlaneShown, showPoint.transform);
+    currentPlaneShown.transform.rotation = planeAngle;
+
+    switch (planeIndex)
     {
-        showPoint = GameObject.Find("ShowPoint");
-        planesSelection[planeIndex].transform.localScale = new Vector3(1, 1, 1) * coef;
-        currentPlaneShown = Instantiate(planesSelection[planeIndex], showPoint.transform);
+      case 0:
+        currentPlaneType = GlobalGameManager.listOfPlanes.basic_plane;
+        break;
 
-        gm = GlobalGameManager.GetInstance();
-        ms = ManagerScene.instance;
+      case 1:
+        currentPlaneType = GlobalGameManager.listOfPlanes.canadaire_plane;
+        break;
+
+      case 2:
+        currentPlaneType = GlobalGameManager.listOfPlanes.delivery_plane;
+        break;
+
+      default:
+        currentPlaneType = GlobalGameManager.listOfPlanes.basic_plane;
+        break;
     }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        currentPlaneShown.transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
-        planeAngle = currentPlaneShown.transform.rotation;
-    }
-
-    public void StartGame()
-    {
-        SaveCurrentPlane();
-
-        gm.SetSelectedPlane(currentPlaneType);
-
-        //SceneManager.LoadScene("Presentation", LoadSceneMode.Single);
-
-        ms.SetMode(ManagerScene.GameMode.Presentation);
-        ms.LoadGameScene();
-
-        //ms.SetMode(ManagerScene.GameMode.Freemode);
-        //ms.LoadGameScene();
-    }
-
-    public void ExitGame()
-    {
-        //ms.Quit();
-    }
-
-    public void NextPlane()
-    {
-        planeIndex++;
-        planeIndex = planeIndex > planesSelection.Length - 1 ? 0 : planeIndex;
-        UpdateShownPlane();
-    }
-
-    public void PreviousPlane()
-    {
-        planeIndex--;
-        planeIndex = planeIndex < 0 ? planesSelection.Length - 1 : planeIndex;
-        UpdateShownPlane();
-    }
-
-    private void UpdateShownPlane()
-    {
-        Destroy(currentPlaneShown);
-        currentPlaneShown = planesSelection[planeIndex];
-        currentPlaneShown.transform.localScale = new Vector3(1, 1, 1) * coef;
-        currentPlaneShown = Instantiate(currentPlaneShown, showPoint.transform);
-        currentPlaneShown.transform.rotation = planeAngle;
-
-        switch (planeIndex)
-        {
-            case 0:
-                currentPlaneType = GlobalGameManager.listOfPlanes.basic_plane;
-                break;
-
-            case 1:
-                currentPlaneType = GlobalGameManager.listOfPlanes.canadaire_plane;
-                break;
-
-            case 2:
-                currentPlaneType = GlobalGameManager.listOfPlanes.delivery_plane;
-                break;
-
-            default:
-                currentPlaneType = GlobalGameManager.listOfPlanes.basic_plane;
-                break;
-        }
-    }
-
-    private void SaveCurrentPlane()
-    {
-        // GameManager.instance.SetPlane(...);
-        Debug.Log("Current plane has been saved in GameManager.");
-    }
+  private void SaveCurrentPlane()
+  {
+    // GameManager.instance.SetPlane(...);
+    Debug.Log("Current plane has been saved in GameManager.");
+  }
 }
