1d4d64a0e393803540bfbc9a3ec3f4a200e52cdd
Mathéo <matheo.galu56@gmail.com>
2021-12-09 17:48:48 +0100

    raycast shit

--- unityProject/Assets/Editor/TerrainGeneratorEditor.cs
+++ unityProject/Assets/Editor/TerrainGeneratorEditor.cs
@@ -5,18 +5,18 @@ using UnityEditor;
 [CustomEditor(typeof(TerrainGenerator))]
 public class TerrainGeneratorEditor : Editor
 {
-    
-	public override void OnInspectorGUI()
-	{
-		TerrainGenerator generator= (TerrainGenerator)target;
 
-		if (DrawDefaultInspector())
-		{
-			
-		}
-		if (GUILayout.Button("Test"))
-		{
-			generator.BlackBox(40, 60, new Vector2(0, 0));
-		}
-	}
+  public override void OnInspectorGUI()
+  {
+    TerrainGenerator generator = (TerrainGenerator)target;
+
+    if (DrawDefaultInspector())
+    {
+
+    }
+    if (GUILayout.Button("Test"))
+    {
+      generator.BlackBox(3000, -3000, new Vector2(0, 0));
+    }
+  }
 }
