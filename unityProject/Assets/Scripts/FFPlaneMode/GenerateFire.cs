c827894cb4cab3843c13a2f22118c575ec425bbf
Mathéo <matheo.galu56@gmail.com>
2022-01-09 15:04:12 -0500

    Auto stash before merge of "main" and "origin/main"

--- unityProject/Assets/Scripts/FFPlaneMode/GenerateFire.cs
+++ unityProject/Assets/Scripts/FFPlaneMode/GenerateFire.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class GenerateFire : MonoBehaviour
 {
-    private GameObject firePrefab;
+  [SerializeField]
+  private float minAltitude = 0.0f;
+  [SerializeField]
+  private float maxAltitude = 0.0f;
+
+  [SerializeField]
+  private GameObject firePrefab;
 
     private Vector3 center;
     private Vector3 size;
@@ -15,16 +21,18 @@ public class GenerateFire : MonoBehaviour
 
     private List<GameObject> fireList = new List<GameObject>();
 
-    void Start()
-    {
-        firePrefab = Resources.Load("Fire") as GameObject;
-        center = transform.position;
-        size = transform.localScale;
+  void Start()
+  {
+    center = transform.position;
+    size = transform.localScale;
 
-        ChangeWaypoint();
-    }
+    ChangeWaypoint();
+  }
 
-    private void FixedUpdate()
+  public void FireSpawn()
+  {
+    int nbOfFire = Random.Range(nbMinOfFire, nbMaxOfFire);
+    for (int i = 0; i < nbOfFire; ++i)
     {
         RaycastHit hit;
         Ray ray = new Ray(transform.position, Vector3.down);
@@ -56,46 +64,36 @@ public class GenerateFire : MonoBehaviour
 
     public void ChangeWaypoint()
     {
-        int nbOfFireDisabled = 0;
-        foreach (GameObject fire in fireList)
-        {
-            if (!fire.activeSelf)
-                nbOfFireDisabled++;
-        }
+      Debug.Log("All the fire are desactived");
+      // Generate random position inside a circle of radius 3000 and center at the current position while the altitude is not between min and max altitude
+      Vector2 randomPos;
+      float altitude;
+      do
+      {
+        randomPos = Random.insideUnitCircle * 1000;
+        altitude = GetAltitude(randomPos.x, randomPos.y);
+      }
+      while (altitude < minAltitude || altitude > maxAltitude);
 
-        if (nbOfFireDisabled == fireList.Count)
-        {
-            Debug.Log("All the fire are desactived");
-            // Generate random position inside a circle of radius 3000 and center at the current position^
-            Vector2 randomPos = Random.insideUnitCircle * 1000;
-            GetAltitude(randomPos.x, randomPos.y);
-            Vector3 newPos = new Vector3(randomPos.x, 1000, randomPos.y);
-            transform.position = newPos;
-        }
+      Vector3 newPos = new Vector3(randomPos.x, 1000, randomPos.y);
+      transform.position = newPos;
+      center = new Vector3(randomPos.x, altitude, randomPos.y);
+      FireSpawn();
     }
 
-    public float GetAltitude(float x, float z)
+  public float GetAltitude(float x, float z)
+  {
+    // Raycast test
+    RaycastHit hit;
+    if (Physics.Raycast(new Vector3(x, 10000, z), Vector3.down, out hit, 50000))
     {
-        // Raycast test
-        RaycastHit hit;
-        if (Physics.Raycast(new Vector3(x, 10000, z), Vector3.down, out hit, 50000, LayerMask.GetMask("Ground")))
-        {
-            return hit.point.y;
-        }
-        else
-        {
-            return 0;
-        }
-        {
-            float raycastDistance = hit.distance;
-            raycastDistance = 10000 - raycastDistance;
-            // Debug raycast result
-            Debug.Log("Raycast hit at " + hit.point + " with height " + raycastDistance);
-            return raycastDistance;
-        }
-
-        Debug.Log("Raycast missed");
-        return 0;
+      Debug.Log("Hit : " + hit.collider.name);
+      Debug.DrawRay(new Vector3(x, 10000, z), Vector3.down * hit.distance, Color.red);
+      float raycastDistance = hit.distance;
+      raycastDistance = 10000 - raycastDistance;
+      // Debug raycast result
+      Debug.Log("Raycast hit at " + hit.point + " with height " + raycastDistance);
+      return raycastDistance;
     }
 
     public void FireUnspawn()
