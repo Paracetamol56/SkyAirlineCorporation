b109b75c237dfe909af20da81561711cda401603
Thibaud CB <thibaud.cb@outlook.fr>
2022-01-09 21:12:45 +0100

    Dont delete dialogue please

--- unityProject/Assets/Scripts/FreeMode/Dialogue.cs
+++ unityProject/Assets/Scripts/FreeMode/Dialogue.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
 
-    public TextMeshProUGUI textComponent;
+    public Text textComponent;
     public string[] lines;
     public float textSpeed;
 
