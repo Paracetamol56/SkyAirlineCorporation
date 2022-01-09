decb42549753d512ba734a21c15da506b37a43b2
Mathéo <matheo.galu56@gmail.com>
2022-01-09 15:06:06 -0500

    tabarnak

--- unityProject/Assets/Scripts/FFPlaneMode/GenerateFire.cs
+++ unityProject/Assets/Scripts/FFPlaneMode/GenerateFire.cs
@@ -12,14 +12,14 @@ public class GenerateFire : MonoBehaviour
   [SerializeField]
   private GameObject firePrefab;
 
-    private Vector3 center;
-    private Vector3 size;
+  private Vector3 center;
+  private Vector3 size;
 
-    private int nbMinOfFire = 4;
-    private int nbMaxOfFire = 8;
-    private bool chunckIsLoad = false;
+  private int nbMinOfFire = 4;
+  private int nbMaxOfFire = 8;
+  private bool chunckIsLoad = false;
 
-    private List<GameObject> fireList = new List<GameObject>();
+  private List<GameObject> fireList = new List<GameObject>();
 
   void Start()
   {
@@ -34,52 +34,30 @@ public class GenerateFire : MonoBehaviour
     int nbOfFire = Random.Range(nbMinOfFire, nbMaxOfFire);
     for (int i = 0; i < nbOfFire; ++i)
     {
-        RaycastHit hit;
-        Ray ray = new Ray(transform.position, Vector3.down);
-
-        bool grounded = Physics.Raycast(ray, out hit);
-        if (grounded && hit.transform.tag.Equals("Ground"))
-        {
-            Debug.Log("touch");
-            //FireSpawn();
-            if (!chunckIsLoad) chunckIsLoad = true;
-        }
-        else if (chunckIsLoad)
-        {
-            chunckIsLoad = false;
-            FireUnspawn();
-        }
+      Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.z / 2, -size.z / 2));
+      GameObject fireCreated = Instantiate(firePrefab, pos, Quaternion.identity);
+      fireList.Add(fireCreated);
     }
+  }
 
-    public void FireSpawn()
+  public void ChangeWaypoint()
+  {
+    Debug.Log("All the fire are desactived");
+    // Generate random position inside a circle of radius 3000 and center at the current position while the altitude is not between min and max altitude
+    Vector2 randomPos;
+    float altitude;
+    do
     {
-        int nbOfFire = Random.Range(nbMinOfFire, nbMaxOfFire);
-        for (int i = 0; i < nbOfFire; ++i)
-        {
-            Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.z / 2, -size.z / 2));
-            GameObject fireCreated = Instantiate(firePrefab, pos, Quaternion.identity);
-            fireList.Add(fireCreated);
-        }
+      randomPos = Random.insideUnitCircle * 1000;
+      altitude = GetAltitude(randomPos.x, randomPos.y);
     }
+    while (altitude < minAltitude || altitude > maxAltitude);
 
-    public void ChangeWaypoint()
-    {
-      Debug.Log("All the fire are desactived");
-      // Generate random position inside a circle of radius 3000 and center at the current position while the altitude is not between min and max altitude
-      Vector2 randomPos;
-      float altitude;
-      do
-      {
-        randomPos = Random.insideUnitCircle * 1000;
-        altitude = GetAltitude(randomPos.x, randomPos.y);
-      }
-      while (altitude < minAltitude || altitude > maxAltitude);
-
-      Vector3 newPos = new Vector3(randomPos.x, 1000, randomPos.y);
-      transform.position = newPos;
-      center = new Vector3(randomPos.x, altitude, randomPos.y);
-      FireSpawn();
-    }
+    Vector3 newPos = new Vector3(randomPos.x, 1000, randomPos.y);
+    transform.position = newPos;
+    center = new Vector3(randomPos.x, altitude, randomPos.y);
+    FireSpawn();
+  }
 
   public float GetAltitude(float x, float z)
   {
@@ -95,12 +73,14 @@ public class GenerateFire : MonoBehaviour
       Debug.Log("Raycast hit at " + hit.point + " with height " + raycastDistance);
       return raycastDistance;
     }
+  }
 
-    public void FireUnspawn()
+  public void FireUnspawn()
+  {
+    foreach (GameObject fire in fireList)
     {
-        foreach (GameObject fire in fireList)
-        {
-            Destroy(fire);
-        }
+      Destroy(fire);
     }
+  }
 }
+
