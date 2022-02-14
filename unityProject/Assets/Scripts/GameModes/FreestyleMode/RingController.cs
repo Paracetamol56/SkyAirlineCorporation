cffd16e6eae1ba1cc4f8a6bdc2d0686fa7604c94
Mathéo <matheo.galu56@gmail.com>
2022-02-14 16:15:21 -0500

    Implemented gate behaviors

--- unityProject/Assets/Scripts/GameModes/FreestyleMode/RingController.cs
+++ unityProject/Assets/Scripts/GameModes/FreestyleMode/RingController.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,22 +7,46 @@ public class RingController : MonoBehaviour
 
     private void Start()
     {
+        // create a new material and give it a random color
+        Material newMaterial = new Material(Shader.Find("Standard"));
+        newMaterial.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+
+        GameObject gate = transform.Find("Gate").gameObject;
+        // Assign the new material to the object first slot
+        {
+            Material[] cachedMaterials = gate.GetComponent<Renderer>().materials;
+            cachedMaterials[0] = newMaterial;
+            gate.GetComponent<Renderer>().materials = cachedMaterials;
+        }
+
         GameObject support = transform.Find("Support").gameObject;
-        support.transform.rotation = Quaternion.Euler(0, 0, 0);
+        support.transform.rotation = Quaternion.Euler(-90, 0, transform.rotation.eulerAngles.y);
+        {
+            Material[] cachedMaterials = support.GetComponent<Renderer>().materials;
+            cachedMaterials[1] = newMaterial;
+            support.GetComponent<Renderer>().materials = cachedMaterials;
+        }
 
         // Get all gameobjects children of the support
         for (int i = 0; i < support.transform.childCount; i++)
         {
-            propellers.Add(support.transform.GetChild(i).gameObject);
+            GameObject propeller = support.transform.GetChild(i).gameObject;
+            {
+                Material[] cachedMaterials = propeller.GetComponent<Renderer>().materials;
+                cachedMaterials[1] = newMaterial;
+                propeller.GetComponent<Renderer>().materials = cachedMaterials;
+            }
+            propellers.Add(propeller);
         }
     }
 
     private void Update()
     {
         // Rotate the propellers
-        for (int i = 0; i < propellers.Count; i++)
+        foreach (GameObject propeller in propellers)
         {
-            propellers[i].transform.Rotate(0, Time.deltaTime, 0);
+            Debug.Log(1);
+            propeller.transform.Rotate(20, 0, 0);
         }
     }
 }
